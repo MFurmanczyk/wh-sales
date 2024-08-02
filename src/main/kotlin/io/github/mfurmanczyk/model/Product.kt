ed9e4c295cc3dbeb141826cf5bbf72e6ea7e9853
MFurmanczyk <maciekf552@gmail.com>
2024-08-02 18:37:40 +0200

    Product table extraction update.

---
+++ src/main/kotlin/io/github/mfurmanczyk/model/Product.kt
@@ -0,0 +1,24 @@
+package io.github.mfurmanczyk.model
+
+import org.apache.spark.sql.types.DataTypes
+import org.apache.spark.sql.types.Metadata
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types.StructType
+
+data class Product(
+    val id: Int,
+    val category_id: Int? = null,
+    val name: String,
+    val price: Float,
+    val desc_product: String? = null
+) {
+    companion object {
+        val SCHEMA = StructType(arrayOf(
+            StructField("id", DataTypes.IntegerType, false, Metadata.empty()),
+            StructField("category_id", DataTypes.IntegerType, true, Metadata.empty()),
+            StructField("name", DataTypes.StringType, false, Metadata.empty()),
+            StructField("price", DataTypes.FloatType, false, Metadata.empty()),
+            StructField("desc_product", DataTypes.StringType, true, Metadata.empty()),
+        ))
+    }
+}
