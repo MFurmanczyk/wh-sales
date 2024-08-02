a9958a5f0adee700698024a221a5fad82f14980d
MFurmanczyk <maciekf552@gmail.com>
2024-08-02 18:56:57 +0200

    PromoRel table extraction update.

---
+++ src/main/kotlin/io/github/mfurmanczyk/jobs/PromoRelExtraction.kt
@@ -0,0 +1,35 @@
+package io.github.mfurmanczyk.jobs
+
+import io.github.mfurmanczyk.database.Database
+import io.github.mfurmanczyk.database.MySqlDatabase
+import io.github.mfurmanczyk.database.configuration.MySqlConfiguration
+import io.github.mfurmanczyk.utils.Scripts
+import org.apache.spark.sql.SaveMode
+import org.jetbrains.kotlinx.spark.api.SparkLogLevel
+import org.jetbrains.kotlinx.spark.api.withSpark
+import java.util.*
+
+fun main() = withSpark(
+    appName = "mysql_extractrion_promo_rel",
+    logLevel = SparkLogLevel.INFO
+) {
+
+    Scripts.startScript(spark)
+
+    val dbConfig = MySqlConfiguration(spark)
+
+    val database: Database = MySqlDatabase(
+        spark = spark,
+        host = dbConfig.host,
+        port = dbConfig.port,
+        database = "db_ecommerce_t",
+        user = dbConfig.user,
+        password = dbConfig.password
+    )
+
+    val orderRelDf = database.readTable("t_promo_rel", Properties())
+
+    orderRelDf.write().mode(SaveMode.Overwrite).parquet("data/intermediate/ext_t_promo_rel.parquet")
+
+    Scripts.endScript(spark)
+}
---
+++ src/main/kotlin/io/github/mfurmanczyk/model/PromoRel.kt
@@ -0,0 +1,22 @@
+package io.github.mfurmanczyk.model
+
+import org.apache.spark.sql.types.DataTypes
+import org.apache.spark.sql.types.Metadata
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types.StructType
+
+data class PromoRel(
+    val promo_id: Int,
+    val product_id: Int? = null,
+    val category_id: Int? = null,
+    val rate_discount: Float
+) {
+    companion object {
+        val SCHEMA = StructType(arrayOf(
+            StructField("promo_id", DataTypes.IntegerType, false, Metadata.empty()),
+            StructField("product_id", DataTypes.IntegerType, true, Metadata.empty()),
+            StructField("category_id", DataTypes.IntegerType, true, Metadata.empty()),
+            StructField("rate_discount", DataTypes.FloatType, false, Metadata.empty()),
+        ))
+    }
+}
