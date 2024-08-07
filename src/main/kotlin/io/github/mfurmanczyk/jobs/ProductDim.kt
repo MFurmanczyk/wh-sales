c57cbd419aca78a73850c854f8cf08f4a5e17631
MFurmanczyk <maciekf552@gmail.com>
2024-08-07 10:21:41 +0200

    ProductDim transformation

---
+++ src/main/kotlin/io/github/mfurmanczyk/jobs/ProductDim.kt
@@ -0,0 +1,35 @@
+package io.github.mfurmanczyk.jobs
+
+import io.github.mfurmanczyk.model.Category
+import io.github.mfurmanczyk.model.Product
+import io.github.mfurmanczyk.utils.Scripts
+import org.apache.spark.sql.SaveMode
+import org.jetbrains.kotlinx.spark.api.*
+
+fun main() = withSpark(
+    appName = "transformation_dim_product",
+    logLevel = SparkLogLevel.INFO
+) {
+
+    Scripts.startScript(spark)
+
+    val productDs = spark.read().parquet("data/intermediate/ext_t_product.parquet").to<Product>()
+    val categoryDs = spark.read().parquet("data/intermediate/ext_t_category.parquet").to<Category>()
+
+    val dimDf = productDs
+        .join(categoryDs, productDs("category_id") eq categoryDs("id"))
+        .select(
+            productDs("id"),
+            productDs("name"),
+            productDs("price"),
+            productDs("desc_product").alias("description"),
+            categoryDs("id").alias("category_id"),
+            categoryDs("name").alias("category_name"),
+            categoryDs("desc_category").alias("category_description"),
+            categoryDs("type_liquidity").alias("liquidity")
+        )
+
+    dimDf.write().mode(SaveMode.Overwrite).parquet("data/target/t_product_dim.parquet")
+
+    Scripts.endScript(spark)
+}
